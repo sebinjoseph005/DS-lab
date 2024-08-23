e7af20d9bea93a5d55636daedc6be5e84de9bf15
Sebin Joseph <josephsebin012@gmail.com>
2024-08-23 16:42:29 +0530

    Update seelction.c

--- seelction.c
+++ seelction.c
@@ -1,41 +1,56 @@
-#include<stdio.h>
-void main(){
-	int i=0,n,j,small,temp;
-	printf("enter the limit");
-	scanf("%d",&n);
-	int a[n];
-	printf("enter the elements");
-	for(i=0; i<n; i++)
-	{
-		scanf("%d",&a[i]);
-	}
-	i=0;
-	while(i<n-1)
-	{
-		small=i;
-		j=i+1;
-		while(j<n)
-		{
-	
-			if(a[small]>a[j])
-			{
-				small=j;
-			}
-			j++;
-		}
-		if(i!=small)
-		{
-			temp=a[i];
-			a[i]=a[small];
-			a[small]=temp;
-		}
-		i++;
-	}
-	printf("the sorted array is\n");
-	i=0;
-	for(i=0; i<n; i++)
-	{
-		printf("%d\t",a[i]);
-	}
+#include <stdio.h>
+void main()
+{
+int i,n,j,small,temp,a[100],t=0,space=0;
+space=7*4;
+t=t+2;
+printf("enter the no of elements");
+t++;
+scanf("%d",&n);
+t++;
+printf("enter the numbers");
+t++;
+for(i=0;i<n;i++)
+{
+t++;
+scanf("%d",&a[i]);
+space+=4;
 }
-		
+t=t+1;
+for(i=0;i<n-1;i++)
+{
+t++;
+small=i;
+for(j=i+1;j<n;j++)
+{
+t++;
+if(a[j]<a[small])
+{
+t++;
+small=j;
+j++;
+}
+}
+t+=1;
+if(small!=i)
+{
+t++;
+temp=a[i];
+a[i]=a[small];
+a[small]=temp;
+t++;
+}
+}
+t=t+1;
+printf("sorted array:\n");
+t++;
+for(i=0;i<n;i++)
+{
+t++;
+printf("%d\t",a[i]);
+t++;
+}
+printf("\nspace complexity %d",space);
+t++;
+printf("\ntime complexity %d",t++);
+}		
