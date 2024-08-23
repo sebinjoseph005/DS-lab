318eaecc234f99bea550ed2c7476ead188cc4db4
Sebin Joseph <josephsebin012@gmail.com>
2024-08-23 16:56:00 +0530

    Update binary.c

--- binary.c
+++ binary.c
@@ -1,61 +1,81 @@
+//This code performs binary search on a sorted array and calculates time and space complexity
 #include<stdio.h>
-void main()
-{
-	int left=0,n,x,flag=0,time,mid,i,right;
-	time++;
-	printf("enter the values\n");
-	time++;
-	scanf("%d",&n);
-	time++;
-	int a[n];
-	printf("enter the array elments");
-	time++;
-	for(i=0; i<n; i++)
-	{
-		time++;
-		scanf("%d",&a[i]);
-		time++;
-	}
-	time++;
-	printf("enter the element to search");
-	time++;
-	scanf("%d",&x);
-	time++;
-	right=n-1;
-	while(left<=right)
-	{
-		mid=(left+right)/2;
-		time++;
-		time++;
-		time++;
-		if(x==a[mid])
-		{
-			printf("element is present at%d",mid+1);
-			time++;
-			flag=1;
-			time++;
-			break;
-		}
-		else if(x>a[mid])
-		{
-			left=mid+1;
-			time++;
-		}
-		else if(x<a[mid])
-		{
-			right=mid-1;
-			time++;
-		}
-	}
-	time++;
-	if(flag==0)
-	printf("element is  found");
-	time++;
-	printf("\n space complexity=%d",28+(4*n));
-	time++;
-	time++;
-	printf("\n time complexity=%d",time);
+ int main()
+ {
+    int n;          //Size of the array
+    int A[20];      //Array used to take values
+    int i;          //Variable incremented in for loop 
+    int j;          //Variable incremented in for loop
+    int temp;       //Variable used to swap 2 array elements in buble sort loop(line - 19 to 31)
+    int time = 0;   //Variable to track time complexity of the code
+    int space = 0;  //Variable to track space complexity of the code
+    time += 2;
+ 
+ printf("Enter the size of Array: ");
+ scanf("%d",&n);
+ time += 2;
+
+//loop to construct array
+ printf("Enter %d numbers in ascending order : ",n);
+   for(i=0;i<n;i++)
+    {
+       scanf("%d",&A[i]);
+       time += 2;
+    }
+    time += 2;
+
+
+//begining of binary search
+    int left = 0;       //used to indicate left most element of the part of the arraay that is being searched
+    int right = n-1;    //used to indicate right most element of the part of the arraay that is being searched
+    int mid;            //number used to indicate middle of the part of arraay that is being searched 
+    int x;              //x is the number to be searched
+    int flag = 0;       //variabe used to indicate search element is found in the array
+    time += 3;
+ 
+ printf("Enter number to be searched\n");
+ scanf("%d",&x);
+    if(x < A[left] || x > A[right] ){
+      printf("%d is not found in the array ",x);
+      return 0;
+      time += 3;
+    }
+
+//Binary search loop
+ while(left <= right){
+    mid = (left + right)/2;
+    if(A[mid] == x)
+    {
+        flag = 2;    //Indicates that search element is found
+        break;
+        time += 3;
+    }
+    else if (x < A[mid])
+    {
+        right = mid - 1;
+        time += 2;
+    }
+    else if (x > A[mid])
+    {
+        left = mid + 1;
+        time += 2;
+    }  
+  }  
+
+    //Result  
+        if(flag == 2){
+            printf("%d is found in the array at position %d\n",x,mid+1);
+            time += 2;
+         }
+        if(flag == 0){
+            printf("%d is not found in the array \n",x);
+            time += 2;         
+         }
+
+ time += 3;
+ space = (11 * 4) + (n * 4);
+ printf("Space complexity = %d\nTime complexity = %d\n",space,time);
+ return 0;
 }
-		
 
 
