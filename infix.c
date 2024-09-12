9326ffbd94ebb31c211a0af24b2fae3ef056148d
Sebin Joseph <josephsebin012@gmail.com>
2024-09-12 12:14:53 +0530

    Add files via upload

---
+++ infix.c
@@ -0,0 +1,76 @@
+#include #include #include #include
+const int MAX = 100;
+
+char stack [100];
+int top = -1;
+
+void push(char c) {
+if (top == MAX - 1) {
+printf("Stack overflow\n");
+exit(1);
+}
+stack[++top] = c;
+}
+
+char pop() {
+if (top == -1) {
+printf("Stack underflow\n");
+exit(1);
+}
+return stack[top--];
+}
+
+int precedence(char c) {
+if (c == '+' || c == '-') {
+return 1;
+} else if (c == '*' || c == '/') {
+return 2;
+} else {
+return 0;
+}
+}
+
+void infixToPostfix(char infix[], char postfix[])
+{
+int i, j = 0;
+
+for (i = 0; i < strlen(infix); i++) {
+if (isspace(infix[i])) {
+continue;
+}
+
+if (isalnum(infix[i])) {
+postfix[j++] = infix[i];
+} else if (infix[i] == '(') {
+push(infix[i]);
+} else if (infix[i] == ')') {
+while (stack[top] != '(') {
+postfix[j++] = pop();
+}
+pop();
+} else {
+while (top != -1 && precedence(stack[top]) >= precedence(infix[i])) {
+postfix[j++] = pop();
+}
+push(infix[i]);
+}
+}
+
+while (top != -1) {
+postfix[j++] = pop();
+}
+postfix[j] = '\0';
+}
+
+int main() {
+char infix[MAX], postfix[MAX];
+
+printf("Enter infix expression: ");
+fgets(infix, MAX, stdin);
+infix[strcspn(infix, "\n")] = 0;
+infixToPostfix(infix, postfix);
+
+printf("Postfix expression: %s\n", postfix);
+
+return 0;
+}
